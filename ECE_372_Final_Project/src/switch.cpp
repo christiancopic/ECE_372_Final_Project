4659c989ea3d74967aeb5b30f1d7647e72e42809
MeganMickey <67599197+MeganMickey@users.noreply.github.com>
2021-11-30 10:34:18 -0700

    Update switch.cpp

--- ECE_372_Final_Project/src/switch.cpp
+++ ECE_372_Final_Project/src/switch.cpp
@@ -1,15 +1,18 @@
-#include<Arduino.h>
-#include<avr/io.h>
-#include "switch.h"
-void initSwitchPD0(){
-    
-    DDRD &= ~(1<<DDD0);      //Set pin direction as input (pin 21 on board)
-    PORTD |= (1<<PORTD0);  // enble the pull up resistor 
-    
-    // SET UP EXTERNAL INTERRUPT
-    EICRA &= ~(1<<ISC01); // the change in any edge of INT0 is enble  [A reigister is used for 0:3]
-    EICRA |= (1<<ISC00);
+// Description: This file implements the initialization of an external
+// switch.
+//----------------------------------------------------------------------//
 
-    EIMSK |= (1<<INT0); // external INTO is enable   ( if switch to other edge, this register must be disable first)
-    
+#include "switch.h"
+#include <avr/io.h>
+
+/*
+ * Initializes pull-up resistor on PB3 and sets it into input mode
+ */
+void initSwitchPB3(){
+    DDRB &= ~(1<<DDB3);   //data direction to input
+    PORTB |= (1<<PORTB3);    //enable pullup resistor
+
+
+    PCICR |= (1<<PCIE0);   //enable PCINT0-7
+    PCMSK0 |= (1<<PCINT3);   //enable PCINT 3
 }
