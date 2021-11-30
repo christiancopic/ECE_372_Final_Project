ce29814071b99ed226420f54bcfe8bd74e213c48
MeganMickey <67599197+MeganMickey@users.noreply.github.com>
2021-11-30 09:51:54 -0700

    Update main.cpp

--- ECE_372_Final_Project/src/main.cpp
+++ ECE_372_Final_Project/src/main.cpp
@@ -2,17 +2,22 @@
 #include <stdio.h>
 #include "lcd.h"
 #include "timer.h"
-#include "i2c.h"
 #include <avr/io.h>
 #include <string.h>
+#include <time.h>
 
 
-//Fix this!!
-//#define SLA 0x11 //Lidar device address//
-#define PWR_MGMT 0x6B
-#define WAKEUP 0x00
-//#define SL_INTERFERENCE 0x22
+#define echoPin 19   //attach pin D2 of Arduino to pin Echo of HC-SR04
+#define trigPin 18   //attach pin D3 of Arduino to pin Trig of HC-SR04
 
+long duration;
+int distance;
+
+long INIT_DISTANCE = 150;
+
+double fastest_time = 0.0;
+clock_t curr_start;
+clock_t curr_end;
 
 //Define set of states for lap state
 typedef enum stateEnum{
@@ -24,73 +29,104 @@ typedef enum stateEnum{
 
 volatile stateType state = wait_start;
 
+void initUSS(){
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  Serial.begin(9600);
+}
+
 
 int main(){
   //initialize everything
   initTimer0();
   initTimer1();
   initLCD();
-  //TODO: initialize Lidar???
+  initUSS();
+  
+  double diff;
+
 
   Serial.begin(9600); //baud rate
   sei();
-  //initI2C();
 
-  //variable to hold whether the lidar senses something
-  //unsigned int interference = 0;   //0 means nothing sensed
-
-  // StartI2C_Trans(SLA);
-  // Write(PWR_MGMT);
-  // Write(WAKEUP);
 
   moveCursor(0,0);
   writeString("Fastest Time:");
-  char times[50];
-  double time = 14.2343543;
+
   
-  dtostrf(time, 4, 3, times);
-  //sprintf(times, "%f", time);
-  Serial.print(times);
-
-  moveCursor(1,0);
-  writeString(times);
-
-  double fastest_time = 25.7;
-  double curr_time = 0.0;
 
   while(1){
-    //delayMs(1000);
+    //Clear trigPin condition
+    digitalWrite(trigPin, LOW);
+    delayUs(2);
+    //Set the trigPin HIGH
+    digitalWrite(trigPin, HIGH);
+    delayUs(10);
+    digitalWrite(trigPin, LOW);
+    //Read echoPin, returns sound wave travel time in Us
+    duration = pulseIn(echoPin, HIGH);
+    //Calculate Distance
+    distance = duration*0.034/2;
 
-    //Read_from(SLA, SL_INTERFERENCE);
-    //interference = Read_data();
+    Serial.print("Distance: ");
+    Serial.print(distance);
+    Serial.println(" cm");
+    //Serial.print(state);
+
+
+    if (distance < INIT_DISTANCE){
+      if(state == wait_start){
+        //TODO: start timer
+        state = pass_start;
+      }
+      else if(state == wait_finish){
+        curr_end = clock();
+        diff = curr_end-curr_start;
+        curr_start = curr_end;
+        Serial.println(diff);
+        state = pass_finish;
+        //TODO: restart timer
+      }
+    }
+
 
     switch(state){
       case wait_start:
         break;
       case pass_start:
-        delayMs(3000);
+        curr_start = clock(); 
+        delayMs(3000);  //Calculate vehicle speed to get this time
         state = wait_finish;
         break;
       case wait_finish:
         break;
       case pass_finish:
-        if(curr_time > fastest_time){
-          fastest_time = curr_time;
+        if(diff < fastest_time){
+          fastest_time = diff;
           //convert fastestTime to String
-          char *timeString;
-          sprintf(timeString, "%f", fastest_time);
-          moveCursor(0,1);
+          char timeString[50];
+          dtostrf(fastest_time, 4, 3, timeString);
+          //Display on LCD
+          moveCursor(1,0);
           writeString(timeString);
-          moveCursor(8,1);
+          moveCursor(1,8);
           writeString("s");
+          //TODO: Display smiley face
         }
-        delayMs(3000);
-        state = wait_start;
+        else{
+          //TODO: Display frownie face
+        }
+        delayMs(3000);  //Calculate vehicle speed to get this time
+        state = wait_finish;
         break;
     }
-
-    //StopI2C_Trans();
+    
   }
 
   return 0;
+}
+
+ISR(PCINT0_vect){
+  state = wait_start;
+  fastest_time = 0.0;
 }
