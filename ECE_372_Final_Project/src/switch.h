8a72192c3607bb393a78951962b9f72a471df647
MeganMickey <67599197+MeganMickey@users.noreply.github.com>
2021-11-30 10:34:42 -0700

    Update switch.h

--- ECE_372_Final_Project/src/switch.h
+++ ECE_372_Final_Project/src/switch.h
@@ -1,4 +1,9 @@
+// Description: Function prototypes for switch.cpp
+//----------------------------------------------------------------------//
+
 #ifndef SWITCH_H
 #define SWITCH_H
-void initSwitchPD0();
+
+void initSwitchPB3();
+
 #endif
