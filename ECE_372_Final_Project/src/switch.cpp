3c982aa121bcef549a322800cb77c0d81042658a
MeganMickey <67599197+MeganMickey@users.noreply.github.com>
2021-12-04 13:22:21 -0700

    Update switch.cpp

--- ECE_372_Final_Project/src/switch.cpp
+++ ECE_372_Final_Project/src/switch.cpp
@@ -6,13 +6,13 @@
 #include <avr/io.h>
 
 /*
- * Initializes pull-up resistor on PB2 and sets it into input mode
+ * Initializes pull-up resistor on PB3 and sets it into input mode
  */
-void initSwitchPB2(){
-    DDRB &= ~(1<<DDB2);   //data direction to input
-    PORTB |= (1<<PORTB2);    //enable pullup resistor
+void initSwitchPB7(){
+    DDRB &= ~(1<<DDB7);   //data direction to input
+    PORTB |= (1<<PORTB7);    //enable pullup resistor
 
 
     PCICR |= (1<<PCIE0);   //enable PCINT0-7
-    PCMSK0 |= (1<<PCINT2);   //enable PCINT 2
+    PCMSK0 |= (1<<PCINT7);   //enable PCINT 2
 }
